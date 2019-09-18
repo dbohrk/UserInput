56de0eb5708e3426d94950a8054b8b60cc6bf6d7
Dave Bohrk <dave@ionicsecurity.com>
2019-09-18 17:22:40 -0400

    Initial commit

---
+++ src/corp/kahuna/Main.java
@@ -0,0 +1,20 @@
+package corp.kahuna;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+        System.out.println("How old are you?");
+        int age = Integer.parseInt(scanner.nextLine());
+        System.out.println(name + " is " + age + " years old");
+
+        scanner.close();
+    }
+}
